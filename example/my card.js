8d05457e51711bc26c5292944fa21e3d9ab8045e
studentpurpose1920 <studentpurpose1920@gmail.com>
2025-09-17 12:24:25 +0530

    example

---
+++ example/my card.js
@@ -0,0 +1,6 @@
+let nameinput=document.getElementById("name");
+let branchinput=document.getElementById("branch");
+let numberinput=document.getElementById("phn no");
+let photoinput=document.getElementById("photo");
+let profileimage=null;
+let btninput=document.getElementById("download");
